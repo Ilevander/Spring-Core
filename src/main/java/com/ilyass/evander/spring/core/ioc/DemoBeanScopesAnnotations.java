9344e1384cf0d5b559950bf072168ac013f78369
dell <dell@DESKTOP-4P8U8AR>
2024-03-13 22:48:19 +0100

    DemoBeanScopesAnnotations

---
+++ src/main/java/com/ilyass/evander/spring/core/ioc/DemoBeanScopesAnnotations.java
@@ -0,0 +1,10 @@
+package com.ilyass.evander.spring.core.ioc;
+
+public class DemoBeanScopesAnnotations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
