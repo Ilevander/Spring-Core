c31d160e24818361d2394021cc9e2433909f19b1
dell <dell@DESKTOP-4P8U8AR>
2024-03-13 21:46:38 +0100

    DemoApplicationContext Annotaiton

---
+++ src/main/java/com/ilyass/evander/spring/core/ioc/DemoApplicationContextAnnotation.java
@@ -0,0 +1,10 @@
+package com.ilyass.evander.spring.core.ioc;
+
+public class DemoApplicationContextAnnotation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
